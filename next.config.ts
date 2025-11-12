0be62cc4087d34f6d54418a9f9da7fe234871062
Michael Medzech <info@medzi.de>
2025-11-12 15:11:31 +0100

    Kalender neu2

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '200mb',
     },
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude native binary modules from webpack bundling
+      // These modules will be available in the Node.js runtime on Vercel
+      config.externals.push('ssh2', 'ssh2-sftp-client', 'odbc');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
